0e2acdb3d91fbead898290186806e0dcc2fd7bbb
alpha <rajeevkl007@gmail.com>
2020-03-31 11:38:15 +0530

    updated

--- html/config.php
+++ html/config.php
@@ -2,6 +2,6 @@
    define('DB_SERVER', 'localhost:3306');
    define('DB_USERNAME', 'alpha');
    define('DB_PASSWORD', 'alpha');
-   define('DB_DATABASE', '20200212_AutomatedIrrigation');
+   define('DB_DATABASE', 'Project');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
